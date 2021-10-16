dfb3d64e11e00d0ecdcfb2ee0a813901505d12ca
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-10-16 23:02:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = prompt('Welkom! Wat is je naam?');
+
+alert(`Hey ${name}!`); // Hey + 'ingevoerde naam'!
+
+
+randomNum=Math.floor(Math.random() * 26);
+
+var gok;
+gok= prompt('Voer een nummer in van 0 tot 25?');
+
+if (gok == randomNum){
+   window.alert ('Gefeliciteerd je hebt gewonnen'); 
+} else {
+window.alert('Dat is niet correct');
+};
+
+console.log("dag " + name + " tot de volgende keer");
