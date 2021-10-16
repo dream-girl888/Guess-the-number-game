af4762910a86f6ddb5625b9a23b4d649c4a9e3d0
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-10-16 23:50:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let name = prompt('Welkom! Wat is je naam?');
+let naam;
+let gok;
 
-alert(`Hey ${name}!`); // Hey + 'ingevoerde naam'!
+
+while (naam === undefined || naam === null || naam.length === 0) { //moet altijd wat invoeren anders krijg je steeds weer wat je naam is
+  naam = prompt('Welkom! Wat is je naam?');
+}
+
+alert("Welkom bij Guess the Number " + naam + "!!");
 
 
-randomNum=Math.floor(Math.random() * 26);
+randomNum=Math.floor(Math.random() * 26); //computer genereert 0-25
 
-var gok;
-gok= prompt('Voer een nummer in van 0 tot 25?');
+gok= parseInt(prompt('Voer een nummer in van 0 tot 25?')); // uitsluitend cijfers invoer geen string zoals letter
+
+while (isNaN(gok)) {
+   gok = parseInt(
+     prompt(
+       "Je invoer was geen nummer. Voer alleen nummers in! Probeer het nogmaals"
+     )
+   );
+ }
 
 if (gok == randomNum){
    window.alert ('Gefeliciteerd je hebt gewonnen'); 
@@ -14,4 +27,6 @@ if (gok == randomNum){
 window.alert('Dat is niet correct');
 };
 
-console.log("dag " + name + " tot de volgende keer");
+console.log("dag " + naam + " tot de volgende keer");
+
+
